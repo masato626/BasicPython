ed596aca46db7d1cf78470735ca7daf85b5a9dd5
masato626 <m.takiguchi.22.7.561@4nonome.com>
2024-04-19 19:21:11 +0900

    2個目

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,16 +1,23 @@
-from math import sin 
-import math
+import math 
 
-# --example--
-# print(sin(0))
-# >>> 0
-# -----------
-a=0
-b=math.pi/2
-N=100
-h=b-a/N
-s=0
-for k in range(1,N+1):
-    s+=h/2*(sin(a+(k-1)*h)+sin(a+k*h))
+def f(x):
+    return math.sin(x)
+def g(x):
+    return 4/(1+x**2)
+def h(x):
+    return math.pi**(0.5)*math.exp(-x**2)
+def tra(a,b,N,f):
+    h=(b-a)/N
+    s=0
+    for k in range(1,N+1):
+        s+=h/2*(f(a+(k-1)*h)+f(a+k*h))
 
-print(s)
+    return s
+result1=tra(0,math.pi/2,50,f)
+result2=tra(0,1,100,g)
+result3=tra(-100,100,1000,h)
+
+print("(1)",result1)
+print("(2)",result2)
+print("(3)",result3)
+
