9d25f4aa5ee5e736c17d7a8edc92535004f72a6b
masato626 <m.takiguchi.22.7.561@4nonome.com>
2024-04-19 16:19:10 +0900

    1個目

--- prime_number.py
+++ prime_number.py
@@ -1,14 +1,18 @@
-a = int(61)
-b=int(10)
-if a <= 1:
-    print("素数でない")
-else:
-    is_prime = True
-    for i in range(2, a):
-        if a % i == 0:
-            is_prime = False
-            break
-    if is_prime:
-        print("素数")
-    else:
+a=float(input("a の値を入力: "))
+def prime(a):
+    if a <= 1:
         print("素数でない")
+    elif a!=int:
+        print("素数でない")
+    else:
+        is_prime = True
+        for i in range(2, a):
+            if a % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print("素数")
+        else:
+            print("素数でない")
+
+prime(a)
