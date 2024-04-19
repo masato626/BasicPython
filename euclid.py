2a0f5e19a7a3872e992693bb3af148d03b6bbd19
masato626 <m.takiguchi.22.7.561@4nonome.com>
2024-04-19 12:00:09 +0900

    3個目

--- euclid.py
+++ euclid.py
@@ -1,4 +1,18 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+a = int(input("a の値を入力: "))
+b = int(input("b の値を入力: "))
 
 # TODO
+if a<b:
+    x=b
+    y=a
+else:
+    x=a
+    y=b
+
+while b!=0:
+    q=a//b
+    r=a%b
+    a=b
+    b=r
+
+print(a)
